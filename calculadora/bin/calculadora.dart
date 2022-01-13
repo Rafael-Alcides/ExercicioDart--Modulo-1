c3d88d052ed6bd1190acf21c23c5c503b49b0bd4
Rafael Alcides <rafaelalci10@gmail.com>
2022-01-13 13:43:54 -0300

    add funcao extencao

--- calculadora/bin/calculadora.dart
+++ calculadora/bin/calculadora.dart
@@ -1,35 +1,50 @@
 import 'package:calculadora/calculadora.dart' as calculadora;
-double valor1 = 5.3;
-double valor2 = 4.25;
+
 String operacao = "adicao";
+double numero1 = 5.3;
+double numero2 = 4.25;
 void main() {
-  if (operacao == "adicao") {
-   operacao = ("+").toString();
-  }else if (operacao == "subtracao") {
-     operacao = ("-").toString();
-   }else if (operacao == "multiplicacao") {
-     operacao = ("*").toString();
-   }else if(operacao == "divisao") {
-     operacao = ("/").toString();
-   }
+  final adicaoConta = adicaoConta(numero1, numero2);
+
+  switch (operacao) {
+    case "adicao":
+      print(
+          "O resultado de $numero1 + $numero2 e : $adicaoConta($numero1, $numero2)");
+      break;
+    case "subtracao":
+      operacao = "-";
+      break;
+    case "divisao":
+      operacao = "/";
+      break;
+    case "multiplicacao":
+      operacao = "*";
+      break;
+    default:
+      print("Valor invalido");
+  }
 }
 
-double? adicaoConta() {
-  print(valor1 + valor2);
+double? adicaoConta(double valor1, double valor2) {
+  final resutado = valor1 + valor2;
+  print(resutado);
 }
 
-double? subtracaoConta() {
-  print(valor1 - valor2);
+void subtracaoConta(double valor1, valor2) {
+  final resultado = valor1 - valor2;
+  print(resultado);
 }
 
-double? multiplicacaoConta() {
-  print(valor1 * valor2);
+void multiplicacaoConta(double valor1, valor2) {
+  final resultado = valor1 * valor2;
+  print(resultado);
 }
 
-double? divisaoConta() {
-  print(valor1 / valor2);
+void divisaoConta(double valor1, valor2) {
+  final resultado = valor1 / valor2;
+  print(resultado);
 }
 
-String? result() {
-  print("Resultado de $valor1 $operacao $valor2 :" );
+void resultFinal() {
+  print("O resultado de $numero1 $operacao $numero2 : ");
 }
--- somar_valores/bin/somar_valores.dart
+++ somar_valores/bin/somar_valores.dart
@@ -2,13 +2,20 @@ import 'dart:ffi';
 
 import 'package:somar_valores/somar_valores.dart' as somar_valores;
 
-void main() {
-  calculoDecimais();
+extension cauculadora on double {
+  double plus(double value) {
+    return this + value;
+  }
 }
 
-void calculoDecimais() {
-  double valor1 = 5.3;
-  double valor2 = 4.25;
-  double result = valor1 + valor2;
-  print(result);
+double valor1 = 5.3;
+double valor2 = 4.25;
+
+void main() {
+  print(calculoDecimais());
+}
+
+double calculoDecimais() {
+  return valor1.plus(valor2);
+  
 }
