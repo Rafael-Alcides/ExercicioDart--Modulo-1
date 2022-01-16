54b8615d074bc8e9457f0468e4858d3614b1661c
Rafael Alcides <rafaelalci10@gmail.com>
2022-01-16 09:55:33 -0300

    atividade corrigida

--- somar_valores/bin/somar_valores.dart
+++ somar_valores/bin/somar_valores.dart
@@ -2,20 +2,15 @@ import 'dart:ffi';
 
 import 'package:somar_valores/somar_valores.dart' as somar_valores;
 
-extension cauculadora on double {
-  double plus(double value) {
-    return this + value;
-  }
-}
-
-double valor1 = 5.3;
-double valor2 = 4.25;
+double valor1 = 0, valor2 = 0;
 
 void main() {
-  print(calculoDecimais());
+  valor1 = 5.3;
+  valor2 = 4.25;
+
+  calculoDecimais();
 }
 
-double calculoDecimais() {
-  return valor1.plus(valor2);
-  
+void calculoDecimais() {
+  print(valor1 + valor2);
 }
